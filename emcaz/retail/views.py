352cb29486cfd542029237edd6fa3b8b4b56dfb4
Emmanuel Cazenave <emmanuel.cazenave@gmail.com>
2014-11-07 14:47:28 +0100

    PEP8

--- emcaz/retail/views.py
+++ emcaz/retail/views.py
@@ -2,23 +2,20 @@ from pyramid.renderers import get_renderer
 from pyramid.view import view_config
 from ..resources import Document
 
-#
-#   Default "retail" view
-#
+
 @view_config(
     renderer='templates/home.pt',
     content_type='Root',
-    )
+)
 def home_view(context, request):
     return {}
+
 
 @view_config(
     context=Document,
     renderer='templates/document.pt',
-    )
+)
 def document_view(context, request):
     return {'title': context.title,
             'body': context.body,
-            'master': get_renderer('templates/master.pt').implementation(),
-           }
-
+            'master': get_renderer('templates/master.pt').implementation()}
