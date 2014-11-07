e4726ca6439ad70902696c539809915fd9bc8972
Emmanuel Cazenave <emmanuel.cazenave@gmail.com>
2014-11-07 14:39:53 +0100

    Delete ugly tests and reset wsgi app initialisation.

--- emcaz/__init__.py
+++ emcaz/__init__.py
@@ -3,19 +3,11 @@ from pyramid.config import Configurator
 from substanced.db import root_factory
 
 
-
-def include(config):
-    config.add_static_view('static', 'static', cache_max_age=86400)
-    config.scan()
-
-def includeme(config):
-    config.include('substanced')
-    config.include(include)
-    
-
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings, root_factory=root_factory)
-    config.include(includeme)
+    config.include('substanced')
+    config.add_static_view('static', 'static', cache_max_age=86400)
+    config.scan()
     return config.make_wsgi_app()
